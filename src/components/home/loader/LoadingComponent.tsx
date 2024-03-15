3b5415aad8bbf4ca984998059e64d9216bed458f
sokny2023 <psnpsn667788@gmail.com>
2024-03-15 15:54:05 +0700

    V 1.4 updated loading

--- src/components/home/loader/LoadingComponent.tsx
+++ src/components/home/loader/LoadingComponent.tsx
@@ -4,5 +4,5 @@
 import { Spinner } from 'flowbite-react';
 
 export default function LoadingComponent() {
-  return <Spinner aria-label="Default status example" />;
+  return <Spinner className='mt-6 w-40' aria-label="Extra large spinner example" size="xl" />
 }
